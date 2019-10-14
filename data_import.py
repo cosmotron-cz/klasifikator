670a35310778356933327c15844cba905063fc6e
cosmo-cz <hagara@cosmotron.cz>
2019-10-14 17:44:43 +0200

    add fulltext import

--- data_import.py
+++ data_import.py
@@ -1,9 +1,18 @@
+# coding=utf-8
 import xmltodict
 import xml.etree.ElementTree as etree
 from elasticsearch import Elasticsearch
 from elasticsearch import exceptions
+from elasticsearch_dsl import Search, Q
+from elasticsearch_dsl.utils import AttrDict
 import os
 import tarfile
+from helper.helper import Helper
+from helper.text_extractor import TextExtractor
+from pathlib import Path
+import re
+import time
+from preprocessor import Preprocessor
 
 
 class DataImporter:
@@ -158,6 +167,88 @@ class DataImporter:
                             pass
         print(error_log)
 
+    def import_fulltext(self, index, to_index):
+        pairs = Helper.get_pairs('data/sloucena_id')
+        te = TextExtractor('data/all', 'data/sorted_pages')
+        client = Elasticsearch()
+        s = Search(using=client, index=index)
+        s.execute()
+        for hit in s.scan():
+            hit = hit.to_dict()
+            try:
+                field_001 = hit['001']
+                exists_query = Search(using=client, index=to_index).query(Q({"match": {"id_mzk": field_001}}))
+                response = exists_query.execute()
+                if len(response.hits) != 0:
+                    continue
+                field_072 = hit['072']
+                if isinstance(field_072, (AttrDict, dict)):
+                    field_072 = [field_072]
+                field_080 = hit['080']
+                if isinstance(field_080, (AttrDict, dict)):
+                    field_080 = [field_080]
+                field_650 = hit['650']
+                if isinstance(field_650, (AttrDict, dict)):
+                    field_650 = [field_650]
+                oai = hit['OAI']['a']
+                field_245 = hit['245']
+            except KeyError as ke:
+                print(ke)
+                continue
+            field_020 = hit.get('020', "")
+            if field_020 != "":
+                field_020 = field_020.get('a', '')
+            konspekts = []
+            for field in field_072:
+                if field.get('2', "") == 'Konspekt':
+                    try:
+                        konspekts.append({'category': int(field['9']), 'group': field['a']})
+                    except KeyError as ke:
+                        print(ke)
+                        continue
+                else:
+                    continue
+            if len(konspekts) == 0:
+                continue
+            keywords = []
+            for field in field_650:
+                if field.get('2', '') == 'czenas':
+                    try:
+                        if field['a'] not in keywords:
+                            keywords.append(field['a'])
+                    except KeyError as ke:
+                        print(ke)
+                        continue
+                else:
+                    continue
+            if len(keywords) == 0:
+                continue
+            mdts = []
+            for field in field_080:
+                try:
+                    mdts.append(field['a'])
+                except KeyError as ke:
+                    print(ke)
+                    continue
+            uuids= pairs.get(oai, None)
+            if uuids is None:
+                continue
+            text = ""
+            uuid = ""
+            for ui in uuids:
+                text = te.get_text(ui)
+                if text != "":
+                    uuid = ui
+                    break
+            if text == "":
+                continue
+            pre_text = Preprocessor.preprocess_text_elastic(text, index_to, 'czech')
+            new_dict = {'id_mzk': field_001, 'uuid': uuid, 'oai': oai, 'isbn': field_020, 'text': text, 'czech': pre_text,
+                        'text_pre': "", 'tags_pre': [], 'tags_elastic': [], 'keywords': keywords,
+                        'konpsket': konspekts, 'mdt': mdts,
+                        'title': field_245.get('a', "") + " " + field_245.get('b', "")}
+            client.index(index=to_index, body=new_dict)
+
     @staticmethod
     def is_in_language_dict(record, language):
         field_008 = record.get('008', None)
@@ -272,22 +363,160 @@ class DataImporter:
             tar.close()
 
 
-# index = 'records_nkp_filtered'
-# es = Elasticsearch()
-# # es.indices.delete(index=index)
-# request_body = {
-#     "settings" : {
-#         "number_of_shards": 5,
-#         "number_of_replicas": 1,
-#         "index.mapping.total_fields.limit": 3000
-#     }
-# }
-# es.indices.create(index=index, body=request_body)
-#
-path = 'C:\\Users\\jakub\\Documents\\metadata_nkp.xml'
+index = 'keyword_czech'
+index_to = 'fulltext_mzk'
+es = Elasticsearch()
+es.indices.delete(index=index_to)
+mappings = {
+    "mappings": {
+        "properties": {
+            "title": {
+                "type": "keyword"
+            },
+            "text": {
+                "type": "text",
+                "analyzer": "fulltext_analyzer",
+                "term_vector": "with_positions_offsets_payloads",
+                "store": True,
+            },
+            "czech": {
+                "type": "text",
+                "analyzer": "fulltext_analyzer",
+                "term_vector": "with_positions_offsets_payloads",
+                "store": True
+            },
+            "text_pre": {
+                "type": "text",
+                "term_vector": "with_positions_offsets_payloads",
+                "store": True,
+                "analyzer": "fulltext_analyzer"
+            },
+            "tags_pre": {
+                "type": "text"
+            },
+            "tags_elastic": {
+                "type": "text"
+            },
+            "keywords": {
+                "type": "keyword"
+            },
+            "konspekt": {
+                "fields": {
+                    "category": {
+                        "type": "keyword"
+                    },
+                    "group": {
+                        "type": "keyword"
+                    }
+                }
+            },
+            "mdt": {
+                "type": "keyword"
+            },
+            "id_mzk": {
+                "type": "keyword"
+            },
+            "uuid": {
+                "type": "keyword"
+            },
+            "oai": {
+                "type": "keyword"
+            },
+            "isbn": {
+                "type": "keyword"
+            }
+        }
+    },
+    "settings": {
+        "index": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+        },
+        "index.analyze.max_token_count": 20000,
+        "analysis": {
+            "analyzer": {
+                "czech": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "filter": [
+                        "czech_stop",
+                        "czech_length",
+                        "czech_hunspell",
+                        "lowercase",
+                        "czech_stop",
+                        "unique_on_same_position",
+                        "type_as_payload"
+                    ]
+                }
+            },
+            "fulltext_analyzer": {
+                "type": "custom",
+                "tokenizer": "standard",
+                "filter": [
+                    "lowercase",
+                    "type_as_payload"
+                ]
+            }
+        },
+        "filter": {
+            "czech_hunspell": {
+                "type": "hunspell",
+                "locale": "cs_CZ"
+             },
+            "czech_stop": {
+                "type": "stop",
+                "stopwords": [
+                    "že",
+                    "_czech_"
+                ]
+            },
+            "czech_length": {
+                "type": "length",
+                "min": 2
+            },
+            "unique_on_same_position": {
+                "type": "unique",
+                "only_on_same_position": True
+            }
+        }
+    }
+}
+response = es.index(index=index_to, body=mappings)
+
+# print(response)
+body2 = {
+  "fields" : ["czech"],
+  "offsets" : True,
+  "payloads" : True,
+  "positions" : True,
+  "term_statistics" : True,
+  "field_statistics" : True
+}
+# response = es.termvectors(index_to, id='XA44ym0BMO8lDpHzO3Y8', body=body2)
+# print(response)
+# path = 'C:\\Users\\jakub\\Documents\\metadata_nkp.xml'
 di = DataImporter()
+di.import_fulltext(index, index_to)
 # di.import_metadata(path, index)
 # di.import_part_of_data(path, index, 1088969)
 # di.count_czech_not_tagged(path)
-di.count_tagged_keywords(path)
-#DataImporter.import_texts('C:\\Users\\jakub\\Documents\\ziped1', 'C:\\Users\\jakub\\Documents\\sorted_pages_zip\\sorted_pages', "")
+# di.count_tagged_keywords(path)
+# DataImporter.import_texts('C:\\Users\\jakub\\Documents\\ziped1', 'C:\\Users\\jakub\\Documents\\sorted_pages_zip\\sorted_pages', "")
+
+# all_words = text.split()
+# for n in range(0, len(all_words), 8000):
+#     body3 = {
+#       "analyzer": "czech",
+#       "text": ' '.join(all_words[n: n+8000])
+#     }
+#
+#     response = es.indices.analyze(index=index_to, body=body3)
+# body3 = {
+#       "analyzer": "czech",
+#       "text": ' '.join(all_words[88000: ])
+#     }
+#
+# response = es.indices.analyze(index=index_to, body=body3)
+#
+# print(response)
+
